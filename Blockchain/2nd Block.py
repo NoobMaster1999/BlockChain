abc50eea15796f13055cb83090133ed52428eb5b
Soumen Basak <67274351+NoobMaster1999@users.noreply.github.com>
2024-08-24 17:41:25 +0530

    Add files via upload

---
+++ Blockchain/2nd Block.py
@@ -0,0 +1,9 @@
+import Demo2
+from Demo1 import Block
+
+def next_block(last_block):
+  this_index = last_block.index + 1
+  this_timestamp = Demo2.date.datetime.now()
+  this_data = "Hey! I'm block " + str(this_index)
+  this_hash = last_block.hash
+  return Block(this_index, this_timestamp, this_data, this_hash)
